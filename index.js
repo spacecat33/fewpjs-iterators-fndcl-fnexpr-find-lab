253df6d3c205e9d53f10d3684e2c7aa4efffe11a
Janan Akkad <jananakkad@hotmail.co.uk>
2023-03-19 17:26:33 +0000

    commit tree 4649e2baef8404c2a3c01f4425d8251f4e9f4e31

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+  ]
+  
+  function superbowlWin(array) {
+    let answer = array.find(object => (object.result === "W"))
+    if (answer) {
+    return answer.year
+    }
+    // for (let i = 0; i < array.length; i++) {
+    //   let result = array[i].result
+    //   let year = array[i].year
+    //   debugger
+    //   if (array[i].find("W"))
+    //   console.log(year)
+    //   return year
+    // }
+  }
+  
+  superbowlWin(record)
